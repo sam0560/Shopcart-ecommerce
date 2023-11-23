072a0464a12f582e079f7e01927e449bdc8e5368
Samuel Mensah <samuelmensqh@gmail.com>
2023-11-23 19:51:40 +0000

    *

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
-module.exports = app
+app.use(express.json())
+
+// Importing all the routes
+const products = require('./routes/product')
+
+app.use('/api/v1', products)
+module.exports = app;
