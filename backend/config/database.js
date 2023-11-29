7ecc30a58a15d235da84d6eb754926a958dc4914
Samuel Mensah <samuelmensqh@gmail.com>
2023-11-29 10:48:05 +0000

    Connecting to database

---
+++ backend/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(con => {
+        console.log(`MongoDb Database connected with HOST ${con.connection.host}`);
+    })
+}
+
+module.exports = connectDatabase;
