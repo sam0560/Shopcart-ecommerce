04a991c88887031c87758efa6b9c5d0d24efc225
Samuel Mensah <samuelmensqh@gmail.com>
2024-02-09 17:12:32 +0000

    Adding filter and pagination functionality

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -3,6 +3,9 @@ const Product = require('../models/product')
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsynError = require('../middlewares/catchAsynError')
 
+// Importing api feature
+const APIFeatures = require('../utils/apiFeatures');
+
 // Create new product => /api/v1/admin/product/new
 exports.newProduct = catchAsynError (async (req, res, next) => {
 
@@ -15,14 +18,23 @@ exports.newProduct = catchAsynError (async (req, res, next) => {
 })
 
 
-//  Get all products => /api/v1/products
+//  Get all products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsynError( async (req, res, next) => {
 
-    const products = await Product.find();
+    const resPerPage = 4;
+    const productCount = await Product.countDocuments();
+
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+                    .Search()
+                    .filter()
+                    .pagination(resPerPage)
+    // const products = await Product.find();
+    const products = await apiFeatures.query;
 
     res.status(200).json({
         success: true,
         count: products.length,
+        productCount,
         products
     })
 })
